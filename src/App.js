0820592d23777379d8b9f26c4ac83f2db178d195
tburakonat <tburakonat@gmail.com>
2022-11-03 17:02:36 +0100

    make app component class and add state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import React from 'react';
 import EditWindow from './components/EditWindow';
 
-function App() {
-	return (
-		<div className="ui divided stackable grid container" style={{ marginTop: '50px' }}>
-			<div className="row">
-				<div className="eight wide column">
-					<EditWindow />
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			personal: { first: '', last: '', email: '', phone: '' },
+			educational: { institution: '', subject: '', degree: '', completion: '' },
+			practical: { position: '', company: '', from: '', to: '' },
+		};
+	}
+
+	render() {
+		return (
+			<div className="ui divided stackable grid container" style={{ marginTop: '50px' }}>
+				<div className="row">
+					<div className="eight wide column">
+						<EditWindow />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
